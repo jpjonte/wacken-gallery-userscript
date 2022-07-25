ee778dc04d36a0285343034859eed19c8aa42d28
John Paul Jonte <mail@jpjonte.de>
2022-07-25 12:17:25 +0200

    Add full size image userscript

---
+++ wacken_gallery_full_size.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Wacken Gallery Full Size
+// @namespace    https://jpjonte.de
+// @version      1.0.0
+// @description  Enlarges images and thumbnails in the Wacken Gallery
+// @author       Buddy Jonte
+// @match        http://gallery.wacken.com/index.php*
+// @match        http://gallery.wacken.com/thumbnails.php*
+// @match        http://gallery.wacken.com/displayimage.php*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=wacken.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var elements = document.querySelectorAll('img.image');
+
+    elements.forEach(function (image) {
+        console.log('image', image);
+        var url = image.src;
+
+        image.src = url.replace('normal_', '').replace('thumb_', 'normal_');
+        image.removeAttribute('width');
+        image.removeAttribute('height');
+    });
+})();
